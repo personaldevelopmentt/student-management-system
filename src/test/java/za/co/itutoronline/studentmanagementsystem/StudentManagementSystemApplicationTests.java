71c89596d2c7f2b3cc1219646da1466806a9664a
Ntsako <ntsakoec@gmail.com>
2022-05-31 22:23:18 +0200

    initial commit of standard spring-boot files

---
+++ src/test/java/za/co/itutoronline/studentmanagementsystem/StudentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.itutoronline.studentmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
